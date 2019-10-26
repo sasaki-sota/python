fd18dff99b85ffa9ce3ec106a7e69d21f5e19ea9
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-26 17:00:08 +0900

    Create moziretu.py

---
+++ moziretu.py
@@ -0,0 +1,14 @@
+s = 'test'
+print(s)
+
+print('hello.\nHow are you?')
+
+print("""
+line1
+line2
+line3
+""")
+# ダブルコーデーションを３つ使うことで複数の文字を出力することができるようになる
+
+print('Hi' * 3 + 'Mile.')
+# 様々なものを組み合わせることが可能
