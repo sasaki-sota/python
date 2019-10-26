d92767e5b89c273681344db48d55ad93f02f89fd
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-26 16:50:25 +0900

    Create suti.py

---
+++ suti.py
@@ -0,0 +1,12 @@
+print(2 + 2)
+# 四則演算などはpubyと変わらない
+print(17 // 3)
+# 　商のみを出力することができるようになる
+pie = 3.1419
+round(pie, 2)
+# roundは指定したもののみを出力してくれる
+import math
+result = math.sqrt(25)
+# 数学的なライブラリが存在している
+print(result)
+
