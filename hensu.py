7120ff4c7a4d17717933401ba5f0a33b6207e09a
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-26 16:28:05 +0900

    Update hensu.py

---
+++ hensu.py
@@ -0,0 +1,19 @@
+num = 1 
+# 変数の定義の仕方(数字)
+name = 'Mike'
+# 文字
+is_ok = True
+# ベーレン型で教えてくれる（true or false）
+
+print(num, type(num))
+# typeを指定することによって 'string' 'int'　型などを教えてくれる
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+num = name
+# string型に変更される
+
+print(num, type(num))
+
+# pythonはいちいち型の定義をしなくてもいい
+
