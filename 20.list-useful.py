17e549dfe53c63d5d829e675b6795ba6b6a55b7e
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-27 12:41:57 +0900

    Create 20.list-useful.py

---
+++ 20.list-useful.py
@@ -0,0 +1,15 @@
+seat =  []
+min = 0
+max = 5
+min <= len(seat) < max
+# seatのながさをLenで表す
+
+seat.append('p')
+print(min <= len(seat) < max)
+
+len(seat)
+seat.append('p')
+# 一つ増える
+seat.append('p')
+seat.append('p')
+seat.append('p')
