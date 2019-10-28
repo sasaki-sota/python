40d4be9d321b25c30d5d492d61fc0fc3170a742f
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-28 16:01:54 +0900

    Create 45.enumerate.py

---
+++ 45.enumerate.py
@@ -0,0 +1,8 @@
+i = 0
+for fruit in ['apple', 'banana', 'oarange']:
+    print(i, fruit)
+    i += 1
+
+for i, fruit in enumerate(['apple', 'banana', 'orange']):
+    print(i, fruit)
+# enumerateでiに入れて代入をしてくれる
