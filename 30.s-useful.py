adcd3281da3f4ac3bcf564e896677ece4f031a37
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-28 13:51:50 +0900

    Create 30.s-useful.py

---
+++ 30.s-useful.py
@@ -0,0 +1,9 @@
+my_friends = {'A', 'C', 'D'}
+A_friends = {'B', 'D', 'E', 'F'}
+print(my_friends & A_friends)
+# このようにすると共通の友達を検索することができるようになる
+
+f = ['apple', 'banana', 'apple', 'banana']
+kind = set(f)
+# リストから集合に型を変える
+print(kind)
