44bf99b6344ee63a80b7c93189bc050cab599639
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-28 15:50:01 +0900

    Create 43.for-else.py

---
+++ 43.for-else.py
@@ -0,0 +1,9 @@
+for fruit in ['apple', 'banana', 'oarange']:
+    print(fruit)
+else:
+    print('I ate all?')
+
+    if fruit == 'banana':
+        print('stop eatting')
+        break
+    print(fruit)
