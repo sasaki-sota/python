c85344fbf863f2eced22d6b82159388ccf816f4f
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-28 13:38:23 +0900

    Create 27.g-useful.py

---
+++ 27.g-useful.py
@@ -0,0 +1,12 @@
+l = [ 
+    ['apple', : 100] ,
+    ['banana', : 120] ,
+    ['orange' : 140] ,
+]
+fruits = {
+    'apple' : 100,
+    'banana' :200,
+    'orange' :300,
+}
+# 辞書型で作成
+print(fruits['apple'])
