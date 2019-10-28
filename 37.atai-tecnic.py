2737048207f0e44900c89568f76a7acea980c1e0
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-28 14:39:08 +0900

    Create 37.atai-tecnic.py

---
+++ 37.atai-tecnic.py
@@ -0,0 +1,21 @@
+is_ok = True
+
+if is_ok:
+    print('OK')
+
+is_ok = 1
+
+if is_ok:
+    print('OK')
+else:
+    print('No')
+
+    # ０はfalseそれ以外はtrue
+
+is_ok = ''
+
+if is_ok:
+    print('OK')
+else:
+    print('No')
+
