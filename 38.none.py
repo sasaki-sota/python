23b3e201db7b31be8db912724c1ec649ff00d9e3
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-28 14:45:59 +0900

    Create 38.none.py

---
+++ 38.none.py
@@ -0,0 +1,15 @@
+is_empty = None
+print(is_empty)
+print(help(is_empty))
+
+if is_empty is None:
+    # is_emptyはnone(何もない)かどうか
+    print('None')
+
+
+if is_empty is not None:
+# これで否定文
+
+print(1 == True)
+print(1 is True)
+# 出の場合はオブジェクトによって比較される
