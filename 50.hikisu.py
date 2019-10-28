14f777d78a3e4924ae032dc3b881a290f28e4f03
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-28 17:00:42 +0900

    Create 50.hikisu.py

---
+++ 50.hikisu.py
@@ -0,0 +1,11 @@
+def menu(entree='beef', drink='wine', dessert='ice'):
+    # entreeの中にbeefが格納されている
+    print('entree =',entree)
+    print('drink =',drink)
+    print('dessert =',dessert)
+
+ menu(drink)
+
+
+# menu('beef', 'beer', 'ice')
+# 順序をしっかり確認する日必要がある
