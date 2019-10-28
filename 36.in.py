1f70c4065169f511c493f913ef609df48cdaeea4
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-10-28 14:34:24 +0900

    Create 36.in.py

---
+++ 36.in.py
@@ -0,0 +1,27 @@
+y = [1, 2, 3]
+x = 1
+
+if x in y:
+    print('in')
+# xがyのリストに入っているかどうか
+if 100 not in y:
+    print('not in')
+# notがあるとは言っていない
+
+a = 1
+b = 2
+
+if not a == b:
+    print('Not equal')
+    # aとbが等しくはない
+
+
+is_ok = True
+
+if is_ok:
+    # is_okがtrueである
+    print('Hello')
+
+if not is_ok:
+    print('hello')
+    
